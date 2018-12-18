bbd900bb9a5d22b44bb3c896c1f152946d917f16
ios <ios@iosdeMac.local>
2018-12-18 20:29:32 +0800

    first commit

---
+++ FourthHomework/MyView.swift
@@ -0,0 +1,21 @@
+//
+//  MyView.swift
+//  FourthHomework
+//
+//  Created by sakura on 2018/12/18.
+//  Copyright © 2018年 lishunli. All rights reserved.
+//
+
+import UIKit
+
+class MyView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
